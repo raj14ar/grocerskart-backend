20f2d7555dd21a8353c35800e7f2762cc78479f9
raj14ar <raj14ar@gmail.com>
2021-03-27 19:30:28 +0530

    fix worng post request to get request

--- routes/api/v1/search.js
+++ routes/api/v1/search.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const search = require('../../../controllers/api/v1/search');
 
-router.post('/', search.search);
+router.get('/', search.search);
 router.use('*', function(req, res){
     res.status(404).json({
         message: 'Page Not Found'
