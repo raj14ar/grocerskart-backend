25f3307e2d46d4193a6f77a9757e410286c716b3
raj14ar <raj14ar@gmail.com>
2021-04-03 05:35:40 +0530

    remove backend view

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home');
-router.get('/', homeController.home);
+// router.get('/', homeController.home);
 router.use('/api', require('./api'));
 
 router.use('*', function(req, res){
